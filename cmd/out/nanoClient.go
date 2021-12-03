18b2ae90e2735d1e3de8eb4700ef434c9701e2e2
Stefan Heiden <stefanheiden@gmail.com>
2021-12-03 21:17:17 +0100

    refactoring

---
+++ cmd/out/nanoClient.go
@@ -0,0 +1,136 @@
+package out
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/polpettone/nano-leaf-control/cmd/config"
+	"github.com/polpettone/nano-leaf-control/cmd/models"
+)
+
+func GetState() (string, error) {
+
+	url := config.GetURL()
+
+	req, err := http.NewRequest("GET", url+"/state", nil)
+
+	if err != nil {
+		return "", err
+	}
+
+	client := &http.Client{Timeout: time.Second * 1}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+func stateBody(onValue bool) []byte {
+	onState := models.OnState{
+		On: models.OnStateValue{
+			Value: onValue,
+		},
+	}
+	jsonValue, _ := json.Marshal(onState)
+	return jsonValue
+}
+
+func SetState(state string) (string, error) {
+
+	var jsonValue []byte
+
+	if state == "on" {
+		jsonValue = stateBody(true)
+	} else {
+		jsonValue = stateBody(false)
+	}
+
+	url := config.GetURL()
+
+	req, err := http.NewRequest("PUT", url+"/state", bytes.NewBuffer(jsonValue))
+
+	if err != nil {
+		return "", err
+	}
+
+	client := &http.Client{Timeout: time.Second * 1}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+func brightnessBody(value int64, duration int64) []byte {
+
+	b := models.Brightness{
+		BrightnessValue: models.BrightnessValue{
+			Value:    value,
+			Duration: duration,
+		},
+	}
+
+	jsonValue, _ := json.Marshal(b)
+	return jsonValue
+}
+
+func SetBrightness(value int64, duration int64) (string, error) {
+
+	jsonValue := brightnessBody(value, duration)
+
+	fmt.Printf("set brightness %s", jsonValue)
+
+	url := config.GetURL()
+
+	req, err := http.NewRequest("PUT", url+"/state", bytes.NewBuffer(jsonValue))
+
+	if err != nil {
+		return "", err
+	}
+
+	client := &http.Client{Timeout: time.Second * 1}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
--- cmd/state.go
+++ cmd/state.go
@@ -1,16 +1,10 @@
 package cmd
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"strconv"
-	"time"
 
-	"github.com/polpettone/nano-leaf-control/cmd/config"
-	"github.com/polpettone/nano-leaf-control/cmd/models"
+	"github.com/polpettone/nano-leaf-control/cmd/out"
 	"github.com/spf13/cobra"
 )
 
@@ -30,7 +24,7 @@ func StateCmd() *cobra.Command {
 
 func handleStateCommand(args []string) (string, error) {
 	if len(args) < 1 {
-		state, err := getState()
+		state, err := out.GetState()
 		if err != nil {
 			return "", err
 		}
@@ -38,7 +32,7 @@ func handleStateCommand(args []string) (string, error) {
 	}
 
 	if args[0] == "on" || args[0] == "off" {
-		return setState(args[0])
+		return out.SetState(args[0])
 	}
 
 	if args[0] == "brightness" {
@@ -51,7 +45,7 @@ func handleStateCommand(args []string) (string, error) {
 				return "brightness needs numeric value", nil
 			}
 
-			return setBrightness(value, 0)
+			return out.SetBrightness(value, 0)
 
 		} else {
 			return "brightness needs at least one value", nil
@@ -76,127 +70,4 @@ func handleStateCommand(args []string) (string, error) {
 func init() {
 	c := StateCmd()
 	rootCmd.AddCommand(c)
-}
-
-func getState() (string, error) {
-
-	url := config.GetURL()
-
-	req, err := http.NewRequest("GET", url+"/state", nil)
-
-	if err != nil {
-		return "", err
-	}
-
-	client := &http.Client{Timeout: time.Second * 1}
-	resp, err := client.Do(req)
-
-	if err != nil {
-		return "", err
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return "", err
-	}
-
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
-}
-
-func stateBody(onValue bool) []byte {
-	onState := models.OnState{
-		On: models.OnStateValue{
-			Value: onValue,
-		},
-	}
-	jsonValue, _ := json.Marshal(onState)
-	return jsonValue
-}
-
-func setState(state string) (string, error) {
-
-	var jsonValue []byte
-
-	if state == "on" {
-		jsonValue = stateBody(true)
-	} else {
-		jsonValue = stateBody(false)
-	}
-
-	url := config.GetURL()
-
-	req, err := http.NewRequest("PUT", url+"/state", bytes.NewBuffer(jsonValue))
-
-	if err != nil {
-		return "", err
-	}
-
-	client := &http.Client{Timeout: time.Second * 1}
-	resp, err := client.Do(req)
-
-	if err != nil {
-		return "", err
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
-}
-
-func brightnessBody(value int64, duration int64) []byte {
-
-	b := models.Brightness{
-		BrightnessValue: models.BrightnessValue{
-			Value:    value,
-			Duration: duration,
-		},
-	}
-
-	jsonValue, _ := json.Marshal(b)
-	return jsonValue
-}
-
-func setBrightness(value int64, duration int64) (string, error) {
-
-	jsonValue := brightnessBody(value, duration)
-
-	fmt.Printf("set brightness %s", jsonValue)
-
-	url := config.GetURL()
-
-	req, err := http.NewRequest("PUT", url+"/state", bytes.NewBuffer(jsonValue))
-
-	if err != nil {
-		return "", err
-	}
-
-	client := &http.Client{Timeout: time.Second * 1}
-	resp, err := client.Do(req)
-
-	if err != nil {
-		return "", err
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
 }
